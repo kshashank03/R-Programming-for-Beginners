a77f4c0073001f4638020c573045614ed3200c71
kshashank03 <54222442+kshashank03@users.noreply.github.com>
2021-02-07 01:10:26 -0600

    some content

--- tutorial.R
+++ tutorial.R
@@ -1,5 +1,37 @@
-x <- 1
+# Setup environment using this URL
+# https://medium.com/analytics-vidhya/a-fresh-start-for-r-in-vscode-ec61ed108cf6
 
-print(x)
+# Comments
 
-print("test")
+vec# Simple Calculation
+
+a <- 1 + 2
+
+# Numeric Value
+mynumeric <- 0.2
+my_other_numeric <- 10
+
+# Logical Value
+mylogical <- TRUE
+myotherlogical <- F
+
+# You can use "=" for assignment but it's not recommended
+
+# Characters
+mycharacter <- "my little story"
+myothercharacter <- 'my little story'
+
+# Factors (categorical variables)
+myfactor <- as.factor("female")
+
+# vector
+
+vec1 <- c(1, 2, 3, 4, 5, 6) # C means concantenate
+vec2 <- c(T, F, FALSE, TRUE, F)
+vec3 <- c("a", "b", "c", "d", "e", "f")
+
+vec4 <- as.factor(c("female", "male", "female", "true", "female", "male"))
+
+vec5 <- c(1, TRUE)
+
+mylist <- list(vec1, vec2, vec3, 20, list(1, 2, 3, 4))
